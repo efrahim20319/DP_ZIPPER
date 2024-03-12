e6ec74d31e1cdb3050855c0087e70643b2f70d7b
Efrahim Tamaku <efrahimtks@gmail.com>
2024-03-12 16:31:12 +0000

    Zipping successfully

--- src/main.c
+++ src/main.c
@@ -6,41 +6,61 @@
 
 int main(int argc, char const *argv[])
 {
-     //flags
-    char * username, * usernumer , * usernames, * usernumers;
+    // flags
+    char *username, *usernumer, *usernames, *usernumers;
     username = usernumer = usernames = usernumers = NULL;
+    int hasTheSRCFolder = 0;
     int i = 0;
-    for ( i = 0; i < argc; i++)
+    for (i = 0; i < argc; i++)
     {
-        //Need to be refactored
-        if (strcmp("--username", argv[i]) == 0) {
-            username = argv[i+1];
+        // Needs to be refactored
+        if (strcmp("--username", argv[i]) == 0)
+        {
+            username = argv[i + 1];
             continue;
         }
-        if (strcmp("--usernumber", argv[i]) == 0) {
-            usernumer = argv[i+1];
+        if (strcmp("--usernumber", argv[i]) == 0)
+        {
+            usernumer = argv[i + 1];
             continue;
         }
-        if (strcmp("--usernames", argv[i]) == 0) {
-            usernames = argv[i+1];
+        if (strcmp("--usernames", argv[i]) == 0)
+        {
+            usernames = argv[i + 1];
             continue;
         }
-        if (strcmp("--usernumbers", argv[i]) == 0) {
-            usernumers = argv[i+1];
+        if (strcmp("--usernumbers", argv[i]) == 0)
+        {
+            usernumers = argv[i + 1];
             continue;
         }
     }
     printf("[username: %s]\n[usernumber: %s]\n[usernames: %s]\n[usernumers: %s]\n", username, usernumer, usernames, usernumers);
     system("find * -maxdepth 0 -type d > .dp-zipper-files");
-    FILE * fp = fopen(".dp-zipper-files", "r");
+    FILE *fp = fopen(".dp-zipper-files", "r");
     char fileName[100];
-    if (fp != NULL) { 
-         while (fscanf(fp, "%s", fileName) != EOF) {
-            if (strcmp(fileName, "src")  == 0) {
-                printf("Find It");
+    if (fp != NULL)
+    {
+        while (fscanf(fp, "%s", fileName) != EOF)
+        {
+            if (strcmp(fileName, "src") == 0)
+            {
+                hasTheSRCFolder = 1;
             }
         }
     }
     fclose(fp);
+    if (hasTheSRCFolder)
+    {
+        printf("Source folder found\n");
+        fp = fopen("AUTHORS.txt", "w");
+        fprintf(fp, "a%s;%s", usernumer, username);
+        fclose(fp);
+        system("zip -r SAMPLE src AUTHORS.txt");
+    }
+    else
+    {
+        printf("Couldn't find the source folder!!!");
+    }
     return 0;
 }
