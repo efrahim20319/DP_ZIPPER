8109080cfe0e15e6f85917b17e6b5a67816abc4b
Efrahim Tamaku <efrahimtks@gmail.com>
2024-03-12 14:36:43 +0000

    Basic setup and  Reading all the flags

---
+++ src/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <regex.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[])
+{
+     //flags
+    char * username, * usernumer , * usernames, * usernumers;
+    username = usernumer = usernames = usernumers = NULL;
+    int i = 0;
+    for ( i = 0; i < argc; i++)
+    {
+        //Need to be refactored
+        if (strcmp("--username", argv[i]) == 0) {
+            username = argv[i+1];
+            continue;
+        }
+        if (strcmp("--usernumber", argv[i]) == 0) {
+            usernumer = argv[i+1];
+            continue;
+        }
+        if (strcmp("--usernames", argv[i]) == 0) {
+            usernames = argv[i+1];
+            continue;
+        }
+        if (strcmp("--usernumbers", argv[i]) == 0) {
+            usernumers = argv[i+1];
+            continue;
+        }
+    }
+    printf("[username: %s]\n[usernumber: %s]\n[usernames: %s]\n[usernumers: %s]\n", username, usernumer, usernames, usernumers);
+
+    return 0;
+}
